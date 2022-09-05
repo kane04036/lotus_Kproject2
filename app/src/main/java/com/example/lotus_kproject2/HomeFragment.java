aae72be1434b053289c8d4e1dcc065c68a1d52f1
kane04036 <hi011004@naver.com>
2022-09-06 05:06:01 +0900

    검색화면 및 영화 상세페이지 일부

--- app/src/main/java/com/example/lotus_kproject2/HomeFragment.java
+++ app/src/main/java/com/example/lotus_kproject2/HomeFragment.java
@@ -1,19 +1,31 @@
 package com.example.lotus_kproject2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
+    TextView tvHome;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        tvHome = view.findViewById(R.id.tvHome);
+
+        tvHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), MovieDetailActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
--- app/src/main/java/com/example/lotus_kproject2/MovieDetailActivity.java
+++ app/src/main/java/com/example/lotus_kproject2/MovieDetailActivity.java
@@ -1,4 +1,57 @@
 package com.example.lotus_kproject2;
 
-public class MovieDetailActivity {
+import static android.content.ContentValues.TAG;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class MovieDetailActivity extends AppCompatActivity {
+    ImageView imageMovieDetail;
+    TextView tvMovieNameOfTopBar, tvMovieName, tvYearOfMovie, tvGenre1, tvGenre2, tvGenre3;
+    ImageButton btnBackOfDetailPage, btnMovieBookmark;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_detail);
+
+        imageMovieDetail = findViewById(R.id.imageMovieDetail);
+        tvMovieName = findViewById(R.id.tvMovieName);
+        tvMovieNameOfTopBar = findViewById(R.id.tvMovieNameOfTopBar);
+        tvYearOfMovie = findViewById(R.id.tvYearOfMovie);
+        btnBackOfDetailPage = findViewById(R.id.btnBackOfDetailPage);
+        btnMovieBookmark = findViewById(R.id.btnMovieBookMark);
+
+        imageMovieDetail.setClipToOutline(true);
+
+        btnMovieBookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnMovieBookmark.setImageResource(R.drawable.bookmark_filled_small);
+                Log.d(TAG, "onClick: bookmark: "+ String.valueOf(btnMovieBookmark.getResources()));
+            }
+        });
+    }
+
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+//
+//
+//        return view;
+//    }
+
+
 }
